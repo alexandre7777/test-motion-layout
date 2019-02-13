ab00e323f46c3d8a9afbeb5ae0d53a4f27c209ae
Alexandre <alexandre.genet7@gmail.com>
2019-02-13 22:48:27 +0100

    add coordinator layout todo motion effect

---
+++ app/src/main/java/com/alexandre/testmotion/CoordinatorActivity.kt
@@ -0,0 +1,12 @@
+package com.alexandre.testmotion
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CoordinatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.coordinator_activity)
+    }
+}
--- app/src/main/java/com/alexandre/testmotion/MainActivity.kt
+++ app/src/main/java/com/alexandre/testmotion/MainActivity.kt
@@ -2,11 +2,20 @@ package com.alexandre.testmotion
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.main_activity.*
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        toCoordinator.setOnClickListener {
+            val intent = Intent(this, CoordinatorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
